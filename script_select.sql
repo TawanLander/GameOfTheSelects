5df82843d862e8863f01601ecefa834592df61da
Tawan <tawan.moura@sptech.school>
2026-05-25 15:41:07 -0300

    Arquivos do meu computador

--- script_select.sql
+++ script_select.sql
@@ -1,15 +1,51 @@
 /*
 CONCEITOS:
 FÁCIL:
-
-WHERE, LIKE, ORDER BY, ASC/DESC, DESCRIBE
+WHERE, LIKE, ORDER BY, ASC/DESC, DESCRIBE, AND
 
 MÉDIO:
-
 1 JOIN, FUNÇÕES MATEMÁTICAS (COUNT, AVG, MIN, MAX), CONCAT(), IFNULL()
 
 DIFÍCIL:
-
 +DE 1 JOIN, GROUP BY, LEFT/RIGHT JOIN, SUBQUERY, CASE WHEN
-
 */
+
+
+/* Selecione todos os agentes da missão 'Salvar os dados da SPTech' que começem com 'n' no nome, traga junto as suas especialidades com as colunas especialidade e grau, seus 
+equipamentos com as colunas nome tipo e funcionalidade e seus recursos com as colunas nome, utilidade e qtd
+ trate as colunas para todas terem letra maiúscula como primeira letra, a coluna qtd deve estar escrita como 'Quantidade', todas as palavras acentuadas corretamente*/
+ 
+ select agentes.nome as 'Nome', agentes.serie as 'Código do Agente', missao.nome as 'Missão', especialidade.*, equipamentos.*, recursos.* 
+	from agentes
+		join link_missao_agentes as LMA
+			on agentes.id = LMA.agentes_id
+		join missao
+			on missao.id = LMA.missao_id 
+            
+		join link_especialidades_agentes as LEsA
+			on agentes.id = LEsA.agentes_id
+		join especialidades
+			on especialidades.id = LEsA.especialidades_id
+            
+		join link_recursos_agentes as LRA
+			on agentes.id = LRA.agentes_id
+		join recursos
+			on recursos.id = LRA.recursos.id
+            
+		join link_equipamentos_agentes as LEqA
+			on agentes.id = LEqA.agentes_id
+		join equipamentos
+			on equipamentos.id = LEqA.equipamentos_id
+		
+        where missao.nome = 'Salvar os dados da SPTech' and agentes.nome like 'n%'
+        
+/* Selecione todos os nomes dos agentes que terminem com 'bond' e suas respectivas especialidades que tem grau >= 6*/
+
+/* Traga o nome e o cargo dos envolvidos formatados com a primeira letra maiúscula onde
+nome dos envolvidos que incluam 'a' e que o cargo seja 'diretor' por ordem descrescente */
+
+/* Traga o nome e a descrição dos envolvidos que não são alvos, formate o campo nome e descrição para que ambos fiquem com a primeira letra maiúscula */
+
+/* Traga o nome, a data e o objetivo de uma a missão que acabe em x data e com o objetivo y trate os todas as colunas com a primeira letra maiúscula*/
+
+/* Traga o nome e o objetivo das missões que comecem em 'x' data e que o objetivo contenha 'y', e que ainda não tenha data de finalização da missão */
--- script_tabelas.sql
+++ script_tabelas.sql
@@ -10,61 +10,6 @@ O Agente X foi indicado para uma nova missão, seu alvo fica no XXX, localizado 
 create database selects;
 use selects;
 
-create table agentes(
-id int primary key auto_increment,
-nome varchar(100),
-serie varchar(20)
-);
-
-create table especialidades(
-id int primary key auto_increment,
-especialidade varchar(50),
-grau int,
-constraint chkGrau check (grau in(1, 10))
-);
-
-create table equipamentos(
-id int primary key auto_increment,
-nome varchar(100),
-tipo varchar(25),
-funcionalidade varchar(100),
-quantidade int
-);
-
-create table envolvidos(
-id int primary key auto_increment,
-missao_id INT,
-nome varchar(50),
-cargo varchar(50),
-aka varchar(50),
-descricao varchar(200),
-alvo tinyint
-);
-
-create table missao(
-id int primary key auto_increment,
-nome varchar(50),
-objetivo varchar(100),
-dtInicio datetime,
-dtFim datetime
-);
-
-create table localizacao(
-id int primary key auto_increment,
-pais varchar(50),
-provincia varchar(50),
-cidade varchar(50),
-bairro varchar(50),
-logradouro varchar(80)
-);
-
-create table recurso(
-id int primary key auto_increment,
-nome varchar(50),
-utilidade varchar(50),
-quantidade decimal(10, 2)
-);
-
 create table link_especialidades_agentes (
 id_agentes int,
 id_especialides int,
@@ -78,7 +23,7 @@ id_agentes int,
 id_equipamentos int,
 primary key (id_agentes, id_equipamentos),
 constraint id_agentes_link_equipamentos_agentes foreign key (id_agentes) references agentes(id),
-constraint id_especialidades_link_equipamentos_agentes foreign key (id_equipamentos) references equipamentos(id)
+constraint id_especialidades_link_equipamentos_agentes foreign key (id_especialides) references especialidades(id)
 );
 
 create table link_recursos_agentes (
@@ -86,12 +31,13 @@ id_agentes int,
 id_recursos int,
 primary key (id_agentes, id_recursos),
 constraint id_agentes_link_recursos_agentes foreign key (id_agentes) references agentes(id),
-constraint id_recursos_link_recursos_agentes foreign key (id_recursos) references recurso(id)
+constraint id_recursos_link_recursos_agentes foreign key (id_recursos) references recursos(id)
 );
 
 create table link_missao_agentes (
 id_agentes int,
 id_missao int,
+estadoMissao tinyint,
 primary key (id_agentes, id_missao),
 constraint id_agentes_link_missao_agentes foreign key (id_agentes) references agentes(id),
 constraint id_missao_link_missao_agentes foreign key (id_missao) references missao(id)
@@ -105,3 +51,55 @@ constraint id_missao_link_missao_localizacao foreign key (id_missao) references 
 constraint id_localizacao_link_missao_localizacao foreign key (id_localizacao) references localizacao(id)
 );
 
+create table especialidades(
+id int primary key auto_increment,
+especialidade varchar(50),
+grau int,
+constraint chkGrau check (grau in(1, 10))
+);
+
+create table agentes(
+id int primary key auto_increment,
+nome varchar(100),
+serie varchar(20)
+);
+
+create table equipamentos(
+id int primary key auto_increment,
+nome varchar(100),
+tipo varchar(25),
+funcionalidade varchar(100),
+quantidade int
+);
+
+create table alvos(
+id int primary key auto_increment,
+nome varchar(50),
+cargo varchar(50),
+aka varchar(50)
+);
+
+create table missao(
+id int primary key auto_increment,
+nome varchar(50),
+objetivo varchar(100),
+dtIncio datetime,
+dtFim datetime
+);
+
+create table localizacao(
+id int primary key auto_increment,
+pais varchar(50),
+provincia varchar(50),
+cidade varchar(50),
+bairro varchar(50),
+logradouro varchar(80),
+nome varchar(50)
+);
+
+create table recurso(
+id int primary key auto_increment,
+nome varchar(50),
+utilidade varchar(50),
+qtd decimal(10, 2)
+);
